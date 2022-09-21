56e73f4193c03a5d7a0b546a946bf4ac1ef610da
josephsavesafe <tanawutsave@gmail.com>
2022-09-21 22:51:44 +0700

    first commit

---
+++ src/components/content.js
@@ -0,0 +1,16 @@
+import light from "../image/light.svg";
+// import dark from "../image/dark.svg";
+
+const Content = () => {
+  return (
+    <main>
+      <div>
+        <h1>Myname is Joseph</h1>
+        <p>Darkmode Workshop</p>
+      </div>
+      <img src={light} alt="logo" />
+    </main>
+  );
+};
+
+export default Content;
